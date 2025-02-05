994719dbd4475f832199448abeade52bf376f459
SwanFlutter1993 <swan.dev1993@gmail.com>
2025-02-05 00:33:42 -0800

    first commit

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import external_path_ios_mac
+import file_selector_macos
+import path_provider_foundation
+import photo_manager
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  ExternalPathIosMacPlugin.register(with: registry.registrar(forPlugin: "ExternalPathIosMacPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
+}
